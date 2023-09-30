fa82b6f7d3e7b5ac20233991a055f6eb9cfc9e9a
coreglitch <mail@s3f.ru>
2023-09-30 21:56:08 +0400

    add enemy movement and collision

--- Actors/Enemy.gd
+++ Actors/Enemy.gd
@@ -1,10 +1,15 @@
 extends KinematicBody2D
 
-export var speed = 1000
+export var speed = 100
+var direction = 1
+export var gravity = 100
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	var _velocity = move_and_slide(Vector2(
+		speed * direction,
+		gravity
+	))
