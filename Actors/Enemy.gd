b281fd7b603fa04212e8da83e1ce6a6338f8f53d
coreglitch <mail@s3f.ru>
2023-09-30 21:34:12 +0400

    add empty enemy script

---
+++ Actors/Enemy.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+export var speed = 1000
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
