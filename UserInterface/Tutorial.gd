4a42fc7efef83b8c20a36e8cc08cffa1b25d02bd
Imolodykh <imolodykh@eqvantalab.ru>
2023-10-01 15:24:10 +0300

    start tutorial

--- UserInterface/Tutorial.gd
+++ UserInterface/Tutorial.gd
@@ -1,12 +1,38 @@
 extends Control
 
 onready var text_label = get_node("Label")
-#onready var player_data = get_node("/root/BaseLevel/PlayerData")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-# PlayerData.connect("damaged", self, "update_interface")
-	update_interface()
+const HELLO_AREA = -1
+const WELCOM_AREA = 0
+const COMFORT_AREA = 1
+const UNCOMFORT_AREA = 2
 
-func update_interface():
-	text_label.text = "You are in the comfort zone"
+var ready_to_next_step = true
+var step_number = 0
+var current_area = WELCOM_AREA
+
+var messages = [
+	#[HELLO_AREA, "Hello!"],
+	[WELCOM_AREA, "You are in the comfort zone"],
+	[WELCOM_AREA, "Press <- and -> to move"],
+	[WELCOM_AREA, "Press space to jump"],
+	[WELCOM_AREA, ""],
+	[COMFORT_AREA, "Your comfort zone follow you"],
+	[COMFORT_AREA, "...almost forewer"],
+	[COMFORT_AREA, ""],
+	[UNCOMFORT_AREA, "NOT SHOW THIS"]
+]
+
+func update_interface(message):
+	text_label.text = message
+
+func tutorial_step(step_number):
+	if current_area == messages[step_number][0]:
+		update_interface(messages[step_number][1])
+	else:
+		ready_to_next_step = false
+
+func _on_Timer_timeout() -> void:
+	if ready_to_next_step:
+		tutorial_step(step_number)
+		step_number += 1
