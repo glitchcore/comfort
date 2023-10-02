ad54b64008ed81b3e138d2adfefc5fc9d5c08f74
coreglitch <mail@s3f.ru>
2023-10-02 20:25:32 +0400

    rename health, use local object

--- UserInterface/GameData.gd
+++ UserInterface/GameData.gd
@@ -1,6 +1,5 @@
 extends Control
 
-onready var health = get_node("Label")
 onready var player_data = get_node("/root/BaseLevel/PlayerData")
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +9,6 @@ func _ready() -> void:
 
 func update_interface():
 	if player_data.hp > 0:
-		health.text = "Health: %s" % player_data.hp
+		$Health.text = "Health: %s" % player_data.hp
 	else:
-		health.text = "You died"
+		$Health.text = "You died"
